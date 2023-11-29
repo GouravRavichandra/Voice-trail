fba39a8b6e3200a2bcf4ed9fdd1c85f764dc09e8
Gourav R <75977813+g-gourav-r@users.noreply.github.com>
2023-11-29 21:28:23 +0530

    Update live_predictions.py

--- live_predictions.py
+++ live_predictions.py
@@ -2,61 +2,60 @@ import keras
 import numpy as np
 import librosa
 
-class LivePredictions:
+class livePredictions:
     """
-    Main class for live speech emotion predictions.
+    Main class of the application.
     """
 
     def __init__(self, path, file):
         """
-        Initialize the class with the path to the model and the file to predict.
+        Init method is used to initialize the main parameters.
         """
         self.path = path
         self.file = file
-        self.loaded_model = None
 
     def load_model(self):
         """
-        Load the pre-trained model.
+        Method to load the chosen model.
+        :param path: path to your h5 model.
+        :return: summary of the model with the .summary() function.
         """
         self.loaded_model = keras.models.load_model(self.path)
+        return self.loaded_model.summary()
 
-    def extract_features(self):
+    def makepredictions(self):
         """
-        Extract features from the provided audio file.
+        Method to process the files and create your features.
         """
         data, sampling_rate = librosa.load(self.file)
         mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
         x = np.expand_dims(mfccs, axis=1)
         x = np.expand_dims(x, axis=0)
-        return x
+        predict_x=model.predict(x) 
+        predictions=np.argmax(predict_x,axis=1)
 
-    def make_predictions(self):
-        """
-        Make predictions using the loaded model and extracted features.
-        """
-        if self.loaded_model is None:
-            raise ValueError("Model not loaded. Call load_model() first.")
+        # predictions = (model.predict(x) > 0.5).astype("int32")
+        # (a & b).any()
 
-        features = self.extract_features()
-        predictions = self.loaded_model.model.predict(features)
-        emotion_label = self.convert_class_to_emotion(predictions[0])
-        return emotion_label
+        # predictions = self.loaded_model.predict_classes(x)
+        print("Prediction is", " ", self.convertclasstoemotion(predictions))
 
     @staticmethod
-    def convert_class_to_emotion(pred):
+    def convertclasstoemotion(pred):
         """
-        Convert the predicted class to a human-readable emotion label.
+        Method to convert the predictions (int) into human readable strings.
         """
-        label_conversion = {
-            0: 'neutral',
-            1: 'calm',
-            2: 'happy',
-            3: 'sad',
-            4: 'angry',
-            5: 'fearful',
-            6: 'disgust',
-            7: 'surprised'
-        }
+        
+        label_conversion = {'0': 'neutral',
+                            '1': 'calm',
+                            '2': 'happy',
+                            '3': 'sad',
+                            '4': 'angry',
+                            '5': 'fearful',
+                            '6': 'disgust',
+                            '7': 'surprised'}
 
-        return label_conversion.get(pred, 'unknown')
+        for key, value in label_conversion.items():
+            if int(key) == pred:
+                label = value
+        return label
