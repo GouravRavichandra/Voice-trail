83b0d0bab21da5f3021d9f894ade00d27c005101
Gourav R <75977813+g-gourav-r@users.noreply.github.com>
2023-11-29 21:37:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,9 @@
 import streamlit as st
-
-
-def main():
-    st.title("Speech Emotion Recognition App")
-
-    st.sidebar.header("Settings")
-    model_path = st.sidebar.text_input("Enter Model Path", "SER_model.h5")
-    
-    uploaded_file = st.sidebar.file_uploader("Choose an audio file", type=["wav"])
-
-    if st.sidebar.button("Make Prediction") and uploaded_file is not None:
-        st.audio(uploaded_file, format="audio/wav")
-        try:
-            prediction = make_prediction(model_path, uploaded_file)
-            display_emotion_label(prediction)
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
-
-def make_prediction(model_path, audio_file):
-    pred = livePredictions(model_path, audio_file)
-    pred.load_model()
-    prediction = pred.make_predictions()
-    return prediction
-
-
-def display_emotion_label(prediction):
-    st.write(f"Predicted Emotion: {prediction}")
 import keras
 import numpy as np
 import librosa
 
-class livePredictions:
+class LivePredictions:
     """
     Main class of the application.
     """
@@ -51,7 +24,7 @@ class livePredictions:
         self.loaded_model = keras.models.load_model(self.path)
         return self.loaded_model.summary()
 
-    def makepredictions(self):
+    def make_predictions(self):
         """
         Method to process the files and create your features.
         """
@@ -59,30 +32,50 @@ class livePredictions:
         mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
         x = np.expand_dims(mfccs, axis=1)
         x = np.expand_dims(x, axis=0)
-        predict_x=model.predict(x) 
-        predictions=np.argmax(predict_x,axis=1)
-        print("Prediction is", " ", self.convertclasstoemotion(predictions))
+        predict_x = self.loaded_model.predict(x)
+        prediction = np.argmax(predict_x, axis=1)
+        return self.convert_class_to_emotion(prediction)
 
     @staticmethod
-    def convertclasstoemotion(pred):
+    def convert_class_to_emotion(pred):
         """
-        Method to convert the predictions (int) into human readable strings.
+        Method to convert the predictions (int) into human-readable strings.
         """
-        
-        label_conversion = {'0': 'neutral',
-                            '1': 'calm',
-                            '2': 'happy',
-                            '3': 'sad',
-                            '4': 'angry',
-                            '5': 'fearful',
-                            '6': 'disgust',
-                            '7': 'surprised'}
 
-        for key, value in label_conversion.items():
-            if int(key) == pred:
-                label = value
-        return label
+        label_conversion = {
+            0: 'neutral',
+            1: 'calm',
+            2: 'happy',
+            3: 'sad',
+            4: 'angry',
+            5: 'fearful',
+            6: 'disgust',
+            7: 'surprised'
+        }
 
+        return label_conversion.get(int(pred), 'Unknown')
+
+def main():
+    st.title("Speech Emotion Recognition App")
+
+    st.sidebar.header("Settings")
+    model_path = st.sidebar.text_input("Enter Model Path", "SER_model.h5")
+    
+    uploaded_file = st.sidebar.file_uploader("Choose an audio file", type=["wav"])
+
+    if st.sidebar.button("Make Prediction") and uploaded_file is not None:
+        st.audio(uploaded_file, format="audio/wav")
+        try:
+            prediction = make_prediction(model_path, uploaded_file)
+            st.write(f"Predicted Emotion: {prediction}")
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+
+def make_prediction(model_path, audio_file):
+    pred = LivePredictions(model_path, audio_file)
+    pred.load_model()
+    prediction = pred.make_predictions()
+    return prediction
 
 if __name__ == "__main__":
     main()
