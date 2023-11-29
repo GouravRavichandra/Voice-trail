fb42ec76d273089a16ab05ec7ac7c531938da9d8
Gourav R <75977813+g-gourav-r@users.noreply.github.com>
2023-11-29 21:51:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,57 +3,24 @@ import keras
 import numpy as np
 import librosa
 
-class LivePredictions:
+
+def convert_class_to_emotion(pred):
     """
-    Main class of the application.
+    Method to convert the predictions (int) into human-readable strings.
     """
 
-    def __init__(self, path, file):
-        """
-        Init method is used to initialize the main parameters.
-        """
-        self.path = path
-        self.file = file
+    label_conversion = {
+        0: 'neutral',
+        1: 'calm',
+        2: 'happy',
+        3: 'sad',
+        4: 'angry',
+        5: 'fearful',
+        6: 'disgust',
+        7: 'surprised'
+    }
 
-    def load_model(self):
-        """
-        Method to load the chosen model.
-        :param path: path to your h5 model.
-        :return: summary of the model with the .summary() function.
-        """
-        self.loaded_model = keras.models.load_model(self.path)
-        return self.loaded_model.summary()
-
-    def make_predictions(self):
-        """
-        Method to process the files and create your features.
-        """
-        data, sampling_rate = librosa.load(self.file)
-        mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
-        x = np.expand_dims(mfccs, axis=1)
-        x = np.expand_dims(x, axis=0)
-        predict_x = self.loaded_model.predict(x)
-        prediction = np.argmax(predict_x, axis=1)
-        return self.convert_class_to_emotion(prediction)
-
-    @staticmethod
-    def convert_class_to_emotion(pred):
-        """
-        Method to convert the predictions (int) into human-readable strings.
-        """
-
-        label_conversion = {
-            0: 'neutral',
-            1: 'calm',
-            2: 'happy',
-            3: 'sad',
-            4: 'angry',
-            5: 'fearful',
-            6: 'disgust',
-            7: 'surprised'
-        }
-
-        return label_conversion.get(int(pred), 'Unknown')
+    return label_conversion.get(int(pred), 'Unknown')
 
 def main():
     st.title("Speech Emotion Recognition App")
@@ -72,9 +39,15 @@ def main():
             st.error(f"An error occurred: {str(e)}")
 
 def make_prediction(model_path, audio_file):
-    pred = LivePredictions(model_path, audio_file)
-    pred = pred.load_model()
-    prediction = pred.make_predictions()
+    loaded_model = keras.models.load_model(model_path)
+    summ = loaded_model.summary()
+    data, sampling_rate = librosa.load(audio_file)
+    mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
+    x = np.expand_dims(mfccs, axis=1)
+    x = np.expand_dims(x, axis=0)
+    predict_x = loaded_model.predict(x)
+    prediction = np.argmax(predict_x, axis=1)
+    prediction = convert_class_to_emotion(prediction)
     return prediction
 
 if __name__ == "__main__":
