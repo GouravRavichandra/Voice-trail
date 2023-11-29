6776e2d08ce66f31217e615652c181bc098c1b16
Gourav R <75977813+g-gourav-r@users.noreply.github.com>
2023-11-29 21:33:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# from live_predictions import livePredictions
+from live_predictions import livePredictions
 
 def main():
     st.title("Speech Emotion Recognition App")
@@ -26,68 +26,6 @@ def make_prediction(model_path, audio_file):
 
 def display_emotion_label(prediction):
     st.write(f"Predicted Emotion: {prediction}")
-
-import keras
-import numpy as np
-import librosa
-
-class livePredictions:
-    """
-    Main class of the application.
-    """
-
-    def __init__(self, path, file):
-        """
-        Init method is used to initialize the main parameters.
-        """
-        self.path = path
-        self.file = file
-
-    def load_model(self):
-        """
-        Method to load the chosen model.
-        :param path: path to your h5 model.
-        :return: summary of the model with the .summary() function.
-        """
-        self.loaded_model = keras.models.load_model(self.path)
-        return self.loaded_model.summary()
-
-    def makepredictions(self):
-        """
-        Method to process the files and create your features.
-        """
-        data, sampling_rate = librosa.load(self.file)
-        mfccs = np.mean(librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40).T, axis=0)
-        x = np.expand_dims(mfccs, axis=1)
-        x = np.expand_dims(x, axis=0)
-        predict_x=model.predict(x) 
-        predictions=np.argmax(predict_x,axis=1)
-
-        # predictions = (model.predict(x) > 0.5).astype("int32")
-        # (a & b).any()
-
-        # predictions = self.loaded_model.predict_classes(x)
-        print("Prediction is", " ", self.convertclasstoemotion(predictions))
-
-    @staticmethod
-    def convertclasstoemotion(pred):
-        """
-        Method to convert the predictions (int) into human readable strings.
-        """
-        
-        label_conversion = {'0': 'neutral',
-                            '1': 'calm',
-                            '2': 'happy',
-                            '3': 'sad',
-                            '4': 'angry',
-                            '5': 'fearful',
-                            '6': 'disgust',
-                            '7': 'surprised'}
-
-        for key, value in label_conversion.items():
-            if int(key) == pred:
-                label = value
-        return label
 
 if __name__ == "__main__":
     main()
