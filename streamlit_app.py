b2688917ee3200f0e7cbfa0c40c8c1de31d46474
gourav-ravi <145653975+gourav-ravi@users.noreply.github.com>
2023-11-29 16:24:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,24 @@ def main():
 
     if st.sidebar.button("Make Prediction") and uploaded_file is not None:
         st.audio(uploaded_file, format="audio/wav")
-        prediction = make_prediction(model_path, uploaded_file)
-        display_emotion_label(prediction)
+        try:
+            prediction = make_prediction(model_path, uploaded_file)
+            display_emotion_label(prediction)
+        except Exception as e:
+            st.error(f"An error occurred: {str(e)}")
+
+# def main():
+#     st.title("Speech Emotion Recognition App")
+
+#     st.sidebar.header("Settings")
+#     model_path = st.sidebar.text_input("Enter Model Path", "SER_model.h5")
+    
+#     uploaded_file = st.sidebar.file_uploader("Choose an audio file", type=["wav"])
+
+#     if st.sidebar.button("Make Prediction") and uploaded_file is not None:
+#         st.audio(uploaded_file, format="audio/wav")
+#         prediction = make_prediction(model_path, uploaded_file)
+#         display_emotion_label(prediction)
 
 def make_prediction(model_path, audio_file):
     pred = LivePredictions(model_path, audio_file)
